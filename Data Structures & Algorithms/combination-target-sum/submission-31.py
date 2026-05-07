27592673c1d8f2653419d4910d7cbcd19969fcb8
VANSHMOJIDRA_096 <vansh.17836@sakec.ac.in>
2026-05-07 15:49:18 +0530

    Add: combination-target-sum - submission-31

---
+++ Data Structures & Algorithms/combination-target-sum/submission-31.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self,nums:list[int],target:int) -> list[list[int]]:
+        result = []
+        def solve(i,cc,cs):
+            if cs == target:
+                result.append(cc.copy())
+                return
+            if cs > target:
+                return
+            for num in range(i,len(nums)):
+                cc.append(nums[num])
+                solve(num,cc,cs + nums[num])
+                cc.pop()
+        solve(0,[],0)
+        return result
