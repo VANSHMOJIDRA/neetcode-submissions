87c51bf63a4aded249ac0f7463bfaab34ad307a7
VANSHMOJIDRA_096 <vansh.17836@sakec.ac.in>
2026-09-12 13:21:52 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self,n:int)-> list[int]:
+        dp = [0] * (n+1)
+        dp[0] = 0
+        for i in range(1,n+1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
